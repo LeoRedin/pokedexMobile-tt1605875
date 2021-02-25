9aa360f1fa1529d7988f277bea93b5f5108930e4
Leonardo Redin <leonardoredin@outlook.com>
2021-02-25 18:35:58 -0300

    added more screens

---
+++ app/screens/Notifications/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+function Notifications() {
+  return (
+    <View>
+      <Text>Notificações</Text>
+    </View>
+  )
+}
+
+export { Notifications }
---
+++ app/screens/Settings/index.js
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import { View, Text } from 'react-native'
+
+function Settings() {
+  return (
+    <View>
+      <Text>Confgurações</Text>
+    </View>
+  )
+}
+
+export { Settings }
